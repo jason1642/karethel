b743fb18ee3e6aa468f73fb9d9ce6b55a9bf27a4
Jason Cruz <jason1642@gmail.com>
2021-03-03 10:32:34 -0500

    auth change

--- client/src/components/HomePage/RightColumn/LinkPortfolioNotification.jsx
+++ client/src/components/HomePage/RightColumn/LinkPortfolioNotification.jsx
@@ -4,10 +4,10 @@ import styled from 'styled-components'
 
 const LinkPortfolioNotification = () => {
   const Container = styled.div` display: flex;
-  padding: 2rem 3rem;
-  margin-bottom: 2rem;
-  border-radius: 20px;
-  background-color: #393945;
+    padding: 2rem 3rem;
+    margin-bottom: 2rem;
+    border-radius: 20px;
+    background-color: #393945;
 
   `;
 
@@ -18,22 +18,22 @@ const LinkPortfolioNotification = () => {
     color: #8f94ab;
     `;
   const TextRow = styled.div` display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
+    align-items: flex-start;
+    justify-content: space-between;
   `;
 
   const LinkButton = styled.div` display: flex;
-  align-items: center;
-  font-size: 12px;
-  font-weight: bold;
-  color: #40c4ff;
-  padding: 4px 12px;
-  align-items: center;
-  border-radius: 30px;
-  justify-content: center;
-  margin-left: 12px;
-  min-width: 150px;
-  background-color: #4d505f;
+    align-items: center;
+    font-size: 12px;
+    font-weight: bold;
+    color: #40c4ff;
+    padding: 4px 12px;
+    align-items: center;
+    border-radius: 30px;
+    justify-content: center;
+    margin-left: 12px;
+    min-width: 150px;
+    background-color: #4d505f;
 
   &:hover {
     cursor: pointer;
